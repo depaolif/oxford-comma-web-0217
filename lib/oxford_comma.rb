6455f5ee38a2b0a71e9588aa22e1d6e2dc7414b4
harleqat <harleqat@gmail.com>
2017-01-01 19:51:17 -0500

    tested

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+	if array.size <= 1
+		return array[0]
+	elsif array.size == 2
+		return array.join(" and ")
+	else
+		last = array.pop
+		res = array.join(", ")
+		res += ", and #{last}"
+		return res
+	end
 end
